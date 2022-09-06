958219b0f0ecfecfadfa1ec7a54a451ddce7cefb
Gerrielofo <gerlof.reinink@gmail.com>
2022-09-06 15:52:42 +0200

    Heap Optimization

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
+    public bool onlyDisplayPathGizmos;
     public LayerMask unwalkableMask;
     public Vector2 gridWorldSize;
     public float nodeRadius;
@@ -19,6 +20,14 @@ public class Grid : MonoBehaviour
         gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
 
         CreateGrid();
+    }
+
+    public int MaxSize
+    {
+        get
+        {
+            return gridSizeX * gridSizeY;
+        }
     }
 
     void CreateGrid()
@@ -80,20 +89,35 @@ public class Grid : MonoBehaviour
     {
         Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
 
-        if(grid != null)
+        if (onlyDisplayPathGizmos)
         {
-            foreach(Node n in grid)
+            if(path != null)
             {
-                Gizmos.color = (n.walkable) ? Color.white : Color.red;
-                if(path != null)
+                foreach(Node n in path)
                 {
+                    Gizmos.color = Color.black;
+                    Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - .1f));
+                }
+            }
+        }
+        else
+        {
+            if(grid != null)
+            {
+                foreach(Node n in grid)
+                {
+                 Gizmos.color = (n.walkable) ? Color.white : Color.red;
+                 if(path != null)
+                 {
                     if (path.Contains(n))
                     {
                         Gizmos.color = Color.black;
                     }
+                 }
+                 Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - .1f));
                 }
-                Gizmos.DrawCube(n.worldPos, Vector3.one * (nodeDiameter - .1f));
             }
+
         }
     }
 }
