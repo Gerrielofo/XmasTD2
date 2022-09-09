446aa2a65f91086108a9696b525bfbfcc2030f4f
Xander Veninga <xanderveninga@gmail.com>
2022-09-09 11:49:54 +0200

    begin wave system

---
+++ Assets/Scripts/Scriptable objects/Enemy.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Enemy : ScriptableObject
+{
+    public string name;
+    public int enemyID;
+    public int ammount;
+    public float rate;
+}
