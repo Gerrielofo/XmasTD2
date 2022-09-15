056d46d0c43a0f9577da5cefb3999eb6c154a541
Gerrielofo <gerlof.reinink@gmail.com>
2022-09-15 11:42:08 +0200

    Revert To ShopWheelReindeer

--- Assets/Scripts/ShopButtonController.cs
+++ Assets/Scripts/ShopButtonController.cs
@@ -7,47 +7,51 @@ using TMPro;
 public class ShopButtonController : MonoBehaviour
 {
     public int id;
-    private Animator anim;
     public string itemName;
     public TextMeshProUGUI itemText;
     public Image selectedItem;
     private bool selected;
     public Sprite icon;
 
+    public Color buttonColor;
     
     void Start()
     {
-        anim = GetComponent<Animator>();
+        buttonColor = this.GetComponent<Image>().color;
+        
     }
 
 
     void Update()
     {
-        if (selected)
-        {
-            selectedItem.sprite = icon;
-            itemText.text = itemName;
-        }
+        this.GetComponent<Image>().color = buttonColor;
     }
 
     public void Selected()
     {
         selected = true;
+        selectedItem.sprite = icon;
+        itemText.text = itemName;
+        ShopWheelController.shopID = id;
     }
     public void Deselected()
     {
         selected = false;
+        selectedItem.sprite = null;
+        ShopWheelController.shopID = 0;
     }
 
     public void HoverEnter()
     {
-        anim.SetBool("Hover", true);
         itemText.text = itemName;
+        buttonColor = Color.gray;
+        buttonColor.a = 1f / 2;
     }
 
     public void HoverExit()
     {
-        anim.SetBool("Hover", false);
         itemText.text = "Select Tower";
+        buttonColor = Color.white;
+        buttonColor.a = 1f / 2;
     }
 }
--- Assets/Scripts/ShopWheelController.cs
+++ Assets/Scripts/ShopWheelController.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class ShopWheelController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public GameObject shopWheel;
+    public bool wantShopWheel;
+    public Image selectedItem;
+    public Sprite noImage;
+    public static int shopID;
+    
     void Update()
     {
-        
+        if (Keyboard.current.tabKey.ReadValue() == 1)
+        {
+            wantShopWheel = true;
+
+        }
+        else
+        {
+            wantShopWheel = false;
+        }
+
+        if (wantShopWheel == true)
+            shopWheel.SetActive(true);
+        else
+            shopWheel.SetActive(false);
+
+        switch (shopID)
+        {
+            case 1: //Buy Reindeer
+                Debug.Log("Reindeer selected");
+            break;
+            case 2: //Buy Snowman
+                Debug.Log("SnowMan selected");
+                break;
+            case 3: //Buy Elves
+                Debug.Log("Elves selected");
+                break;
+            case 4: //Buy Yeti
+                Debug.Log("Yeti selected");
+                break;
+            case 0:
+                selectedItem.sprite = noImage;
+                break;
+        }
     }
 }
