97f09f6b1d33adcc8db5722dbfa2c53e53119da4
Xander Veninga <39034729+EliteForce1@users.noreply.github.com>
2022-09-20 13:18:21 +0200

    changes wave system and start new way of input controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject playerCursor;
+    public int cursorSpeed;
+
+    public void Update()
+    {
+        
+    }
+    
+}
