0fca98187e6c4345f13fe69879171216683803c4
Gerrielofo <gerlof.reinink@gmail.com>
2022-10-03 16:22:55 +0200

    MenuFIx

--- Assets/UXUI/MainMenuManager.cs
+++ Assets/UXUI/MainMenuManager.cs
@@ -40,6 +40,22 @@ public class MainMenuManager : MonoBehaviour
         wantGameinfo = !wantGameinfo;
     }
 
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    List<int> widths = new List<int>() { 1920, 1280, 960, 568 };
+    List<int> heights = new List<int>() { 1080, 800, 540, 320 };
+
+    public void SetScreenSize(int index)
+    {
+        bool fullscreen = Screen.fullScreen;
+        int width = widths[index];
+        int height = heights[index];
+        Screen.SetResolution(width, height, fullscreen);
+    }
+
     public void SetFullscreen(bool _fullscreen)
     {
         Screen.fullScreen = _fullscreen;
