82d99b6ab336936b5e058a1e3de99573a145e73d
Gerrielofo <gerlof.reinink@gmail.com>
2022-10-24 10:37:38 +0200

    Revert "Merge branch 'Multiplayer-Branch'"
    This reverts commit f3504d410dd6eb77476db8a94b7b97affa411064, reversing
    changes made to 10cfa07df125dbcd72a820c2fe579b64e6e012e4.

--- Assets/Scripts/TowerID.cs
+++ Assets/Scripts/TowerID.cs
@@ -10,27 +10,18 @@ public class TowerID : MonoBehaviour
     public GameObject towerBlueprint;
     public GameObject playerCursor;
 
-    public BuildManager buildManager;
-
-    private void Start()
-    {
-        buildManager = GameObject.Find("BuildManager").GetComponent<BuildManager>();
-    }
     public void ChangeTower(int _towerID, int _towerCost, int _playerID)
     {
         Debug.Log("Changing Tower");
-        
+        BuildManager buildManager = GetComponent<BuildManager>();
         TowerPlacement towerPlacement = playerCursor.GetComponent<TowerPlacement>();
-        towerPlacement.playerCursor = playerCursor;
         if (_playerID == 1)
         {
-            Debug.Log("changing for player 1");
             switch (_towerID)
             {
                 case 0:
                     towerPrefab = buildManager.towerPrefabsHalloween[_towerID];
                     towerBlueprint = buildManager.towerBlueprintHalloween[_towerID];
-                    Debug.Log("Case set");
                     break;
                 case 1:
                     towerPrefab = buildManager.towerPrefabsHalloween[_towerID];
@@ -50,13 +41,11 @@ public class TowerID : MonoBehaviour
         }
         else if (_playerID == 2)
         {
-            Debug.Log("changing for player 2");
             switch (_towerID)
             {
                 case 0:
                     towerPrefab = buildManager.towerPrefabsXmas[_towerID];
                     towerBlueprint = buildManager.towerBlueprintXmas[_towerID];
-                    Debug.Log("Case set");
                     break;
                 case 1:
                     towerPrefab = buildManager.towerPrefabsXmas[_towerID];
@@ -75,8 +64,7 @@ public class TowerID : MonoBehaviour
             }
         }
 
-        Debug.Log("Tower prefab = " + towerPrefab);
-        Debug.Log("Tower blueprint = " + towerBlueprint);
         towerPlacement.InitTowerPlacement(towerPrefab, towerBlueprint, cost);
+
     }
 }
